1428f9f20711da2b2a115374f225cd381663b9b0
Adrian Gomez <toadrian22@gmail.com>
2015-02-26 16:18:42 -0800

    i

--- gist/clicks.js
+++ gist/clicks.js
@@ -1,2 +1,9 @@
 /*global $*/
 /*jslint sloppy:true, browser: true, white: true*/
+$("h1.page-title").text("My code runs!");
+$(window).on('click', function (e) {
+    $('img.logo').css({left: e.pageX,
+                       top: e.pageY});
+    $('.logo').toggleClass('rotated');
+    $('h2').toggleClass('rotated');
+});
